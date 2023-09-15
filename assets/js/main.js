9fd40333c032750077f9708a18a3ca0a42364ac7
Indrajit <indrajitchandra98@gmail.com>
2023-09-15 13:34:19 +0600

    testimonial slider add

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,9 @@ $(document).ready(function () {
 
   //   counter up jQuery
   $(".counter").counterUp();
+  $('.testimonials').slick({
+    dots: true,
+    prevArrow: `<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>`,
+    nextArrow: `<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>`,
+  });
 });
