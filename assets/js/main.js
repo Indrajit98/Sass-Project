2b489a711741557d32b943b7d56518c0dbbbd89c
Indrajit <indrajitchandra98@gmail.com>
2023-09-17 23:28:32 +0600

    blog section add

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,4 +13,5 @@ $(document).ready(function () {
     prevArrow: `<button type="button" class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>`,
     nextArrow: `<button type="button" class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>`,
   });
+
 });
